7edd48254cce9fa01253b396ea670823cf6798ea
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2025-04-22 21:09:13 +0900

    next config 수정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	plugins: [require("tailwind-scrollbar-hide")],
 };
 
 export default nextConfig;
